93e2f8f84adb351024af153173f41ada6a951c8e
zhangshuai <zhangshuai@attrsense.com>
2024-04-20 11:21:29 +0800

    1、新增模块

---
+++ module_splitties/src/main/java/com/module/splitties/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.module.splitties
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * https://github.com/LouisCAD/Splitties
+ * https://github.com/LouisCAD/Splitties/tree/main/modules/views-dsl
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ module_sqldelight/src/main/java/com/module/sqldelight/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.module.sqldelight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * https://cashapp.github.io/sqldelight/2.0.2/
+ * https://cashapp.github.io/sqldelight/2.0.2/android_sqlite/
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
