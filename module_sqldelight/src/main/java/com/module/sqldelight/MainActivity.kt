c701adbe798e4e5b2ab17cce33728d0291ea91a9
zhangshuai <zhangshuai@attrsense.com>
2024-08-02 18:06:58 +0800

    1、版本管理； 2、自定义.properties文件配置全局常量

--- module_sqldelight/src/main/java/com/module/sqldelight/MainActivity.kt
+++ module_sqldelight/src/main/java/com/module/sqldelight/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.module.sqldelight
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * https://cashapp.github.io/sqldelight/2.0.2/
  * https://cashapp.github.io/sqldelight/2.0.2/android_sqlite/
+ *
+ * 当前版本：https://handstandsam.com/2019/08/23/sqldelight-1-x-quick-start-guide-for-android/
  */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun load(){
+//        AndroidSqliteDriver(
+//            schema = Database,
+//            context = applicationContext,
+//            name = "items.db"
+//        )
     }
 }
--- module_versions_catalog/src/main/java/com/module/versions/catalog/MainActivity.kt
+++ module_versions_catalog/src/main/java/com/module/versions/catalog/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.module.versions.catalog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "提示：${BuildConfig.BASE_HTTP_URL}", Toast.LENGTH_SHORT).show()
     }
 }
