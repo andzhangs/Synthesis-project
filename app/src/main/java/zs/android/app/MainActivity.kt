1f75907741c7d241aa4d5b8ec4b0661a666d4da9
zhangshuai <zhangshuai@attrsense.com>
2024-08-16 11:08:51 +0800

    1、优化文案

--- app/src/main/java/zs/android/app/MainActivity.kt
+++ app/src/main/java/zs/android/app/MainActivity.kt
@@ -3,6 +3,10 @@ package zs.android.app
 import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Path
 import android.graphics.Typeface
 import android.net.Uri
 import android.os.Build
@@ -72,17 +76,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val allFileLaunch=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if (it.resultCode==RESULT_OK){
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    if (Environment.isExternalStorageManager()) {
-                        loadJar()
-                    } else {
-                        Toast.makeText(this, "授权失败!", Toast.LENGTH_SHORT).show()
+        val allFileLaunch =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        if (Environment.isExternalStorageManager()) {
+                            loadJar()
+                        } else {
+                            Toast.makeText(this, "授权失败!", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
-        }
 
         val launch = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
             if (it) {
@@ -96,7 +101,7 @@ class MainActivity : AppCompatActivity() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 if (Environment.isExternalStorageManager()) {
                     loadJar()
-                }else{
+                } else {
                     val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
                     intent.data = Uri.parse("package:${packageName}")
 //                    startActivityForResult(intent, 1024)
@@ -107,11 +112,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        lifecycleScope.launch(Dispatchers.IO){
-            val info=resources.getString(R.string.test_format).format(12,32)
+        lifecycleScope.launch(Dispatchers.IO) {
+            val info = resources.getString(R.string.test_format).format(12, 32)
             if (BuildConfig.DEBUG) {
                 Log.d("print_logs", "MainActivity::onCreate: $info")
             }
+        }
+
+        mDataBinding.acIv.setOnClickListener {
+            val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img_ami)
+            // 创建一个与源 Bitmap 大小相同的画布
+            val croppedBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(croppedBitmap)
+
+
+            //绘制半圆路径
+            val path = Path()
+            path.addArc(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat(), 60f, 220f)
+            canvas.clipPath(path)
+            // 绘制源 Bitmap 到画布上
+            canvas.drawBitmap(bitmap, 0f, 0f, null)
+            mDataBinding.acIv.setImageBitmap(croppedBitmap)
         }
     }
 
@@ -131,9 +152,10 @@ class MainActivity : AppCompatActivity() {
             loadMethod.invoke(instance, "私有方法【load】.")
 
             val appJarClazz = classLoader.loadClass("com.synthesis.modulejar.AppJar")
-            val loadMethod2 = appJarClazz.getDeclaredMethod("toast", Context::class.java, String::class.java)
+            val loadMethod2 =
+                appJarClazz.getDeclaredMethod("toast", Context::class.java, String::class.java)
             loadMethod2.invoke(appJarClazz, this, "静态方法：load")
-        }catch (e:Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
             if (BuildConfig.DEBUG) {
                 Log.e("print_logs", "加载失败: $e")
