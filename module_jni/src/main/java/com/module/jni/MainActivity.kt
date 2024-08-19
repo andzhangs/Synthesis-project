2ed005fc9f82759b3264b901fc5874d94a43e8f3
zhangshuai <zhangshuai@attrsense.com>
2024-08-19 14:31:08 +0800

    1、图片展示灰阶样式

--- module_jni/src/main/java/com/module/jni/MainActivity.kt
+++ module_jni/src/main/java/com/module/jni/MainActivity.kt
@@ -2,6 +2,8 @@ package com.module.jni
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -25,8 +27,34 @@ class MainActivity : AppCompatActivity() {
 
         val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img_640)
         mDataBinding.acIvImg.setImageBitmap(bitmap)
+
         mDataBinding.acIvImg.setOnClickListener {
             mDataBinding.acIvImg.setImageBitmap(handleBitmap(bitmap))
+        }
+
+        var shapeViewSat=0f
+        mDataBinding.acIvShape.setOnClickListener {
+            val matrix = ColorMatrix().apply { setSaturation(shapeViewSat) }
+            val filter = ColorMatrixColorFilter(matrix)
+            mDataBinding.acIvShape.colorFilter = filter
+            shapeViewSat=if (shapeViewSat == 0f) {
+                1f
+            }else{
+                0f
+            }
+        }
+
+
+        var filterViewSat=1f
+        mDataBinding.acIfvImg.setOnClickListener {
+
+            mDataBinding.acIfvImg.saturation = filterViewSat
+
+            filterViewSat=if (filterViewSat == 0f) {
+                1f
+            }else{
+                0f
+            }
         }
     }
 
